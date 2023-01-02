51d3692ad08f503f01e049e86bbc93b35cb1015a
Rekaad <gryradek@o2.pl>
2023-01-02 17:53:30 +0100

    Repo initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [liczba,setLiczba] = useState('');
+  const [wynik, setWynik] = useState('');
+
+ function oblicz(e){
+  const fibo = [0,1];
+
+  for(var i = 2;i<=e;i++){
+    fibo[i] = fibo[i-1] + fibo[i-2];
+  }
+  setWynik(fibo[e])
+  console.log(fibo[e]);
+ }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Obliczanie elementów ciągu Fibonacciego</h1>
+        <h4>Podaj numer elementu: </h4>
+        <input type="text" id='number' onChange={(e)=>{setLiczba(e.target.value)}}/>
+        <br/>
+        <button onClick={()=> {oblicz(liczba)}}>Oblicz</button>
+      </div>
+     <div> Wartość ciągu dla podanego elementu to: {wynik}</div><br/>
+     <div> Kalkulator Fibonacciego <br/> Radosław Młynek <br/> IMST 2.1/3</div>
     </div>
   );
 }
